12d395f560c6a9a98e91f350a5946a914313bba8
Koss <kkosskitchen@gmail.com>
2024-03-19 21:23:27 +0100

    Initial Commit

---
+++ InfoSport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfoSport
+//
+//  Created by Konstantyn Koroban on 19/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
